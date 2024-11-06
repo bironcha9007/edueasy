8ea435c3203ce52fcb349ab56dc4ad546c410c44
Bayron Castillo <bayroncastillo900725@gmail.com>
2024-11-06 14:44:12 -0500

    api login auth0

--- src/components/home/Hprice.jsx
+++ src/components/home/Hprice.jsx
@@ -6,7 +6,7 @@ const Hprice = () => {
   return (
     <>
       <section className="hprice padding">
-        <Heading subtitle="OFERTAS" title="Precios y Paquetes" />
+        <Heading subtitle="OFERTASs" title="Precios y Paquetes" />
         <div className="price container grid">
           <PriceCard />
         </div>
